aeec03c13bcc47cf3a983813e72d60095dd23ada
Marek Dohnal <datgaspkitteh@gmail.com>
2017-11-07 21:31:27 +0100

    Finalized part 8.

--- Bjarne PPP - CH 4 Drill/Practice.cpp
+++ Bjarne PPP - CH 4 Drill/Practice.cpp
@@ -13,45 +13,57 @@ int main()
 	double converted = 0;
 	double largest_converted = 0;
 
+	bool unit_correct = true;
+
 	string unit = " ";
 	string smallest_unit = " ";
 	string largest_unit = " ";
 
 	while (cin >> value >> unit)
 	{
-		if (unit == "m")
-		{
-			converted = value*m_btw_cm;
-		}
-		else if (unit == "in")
-		{
-			converted = value*in_btw_cm;
-		}
-		else if (unit == "ft")
-		{
-			converted = value*in_btw_ft*in_btw_cm;
-		}
-		else if (unit == "cm")
-		{
-			converted = value;
-		}
-		if (iL == 0 && iH == 0)
+				if (unit == "m")
+				{
+					unit_correct = true;
+					converted = value*m_btw_cm;
+				}
+					else if (unit == "in")
+					{
+						unit_correct = true;
+						converted = value*in_btw_cm;
+					}
+						else if (unit == "ft")
+						{
+							unit_correct = true;
+							converted = value*in_btw_ft*in_btw_cm;
+						}
+							else if (unit == "cm")
+							{
+								unit_correct = true;
+								converted = value;
+							}
+								else
+								{
+									unit_correct = false;
+									cout << "Error, invalid or no unit has been entered, please try again: ";
+								}
+
+		if (iL == 0 && iH == 0 && unit_correct == true)
 		{
 			iH = converted;
 			iL = converted;
 			cout << value << " " << unit << " You're a special snowflake first val, yahoo!\n";
 		}
-		else if (converted > iH)
+		else if (converted >= iH && unit_correct == true)
 		{
 			iH = converted;
 			cout << value << " " << unit << ", is the biggest value so far." << '\n';
 		}
-		else if (converted < iL)
+		else if (converted <= iL && unit_correct == true)
 		{
 			iL = converted;
 			cout << value << " " << unit << ", is the smallest value so far." << '\n';
 		}
-		else
+		else if ( iL < converted < iH && unit_correct == true)
 		{
 			cout << value << " " << unit << ", nothing." << '\n';
 		}
